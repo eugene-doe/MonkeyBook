804140a8c77b53537d6d908d2eac237645d4851b
eugene-doe <dobryanskiy@mail.ru>
2014-12-26 09:31:37 +0200

    More features, bugs fixed

--- MonkeyBook/views.py
+++ MonkeyBook/views.py
@@ -9,42 +9,19 @@ from MonkeyBook.forms import *
 from MonkeyBook.models import db
 import os
 
+## Some views may require two monkey objects: the logged in monkey and some other monkey.
+## For the sake of clarity, the logged in monkey is referred to as 'monkey_self' and any
+## other monkey (such as a monkey that has not yet logged in or a monkey whose profile we
+## are viewing) as 'monkey'.
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not 'id' in session:
-            session['next'] = request.url
+            session['next'] = request.url # Because appending next URL to the current one looks ugly
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
-
-@app.route('/')
-def index():
-    monkey = None
-    if 'id' in session:
-        monkey = Monkey.query.filter_by(id = session['id']).first()
-    return render_template('index.html', monkey=monkey)
-
-@app.route('/<int:monkey_id>')
-@login_required
-def profile(monkey_id=None):
-    if monkey_id is None:
-        monkey_id = session['id']
-
-    monkey = Monkey.query.filter_by(id = monkey_id).first()
-
-    # The following lists keep logic out of the templates
-    # and keep the model simple at the same time:
-
-    mutual_friends = set(monkey.friends).intersection(monkey.friend_of)
-    other_friends = set(monkey.friends).difference(monkey.friend_of)
-    also_friend_of = set(monkey.friend_of).difference(monkey.friends)
-
-    return render_template('profile.html',
-                            monkey=monkey,
-                            mutual_friends=mutual_friends,
-                            other_friends=other_friends,
-                            also_friend_of=also_friend_of)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,31 +53,65 @@ def logout():
     session.pop('id', None)
     return redirect(url_for('index'))
 
+@app.route('/')
+def index():
+    monkey_self = None
+    if 'id' in session:
+        monkey_self = Monkey.query.filter_by(id = session['id']).first()
+    return render_template('index.html', monkey_self=monkey_self)
+
+@app.route('/<int:monkey_id>')
+@login_required
+def profile(monkey_id):
+    monkey_self = Monkey.query.filter_by(id = session['id']).first()
+
+    if monkey_id == session['id']:
+        # Viewing your own profile
+        monkey = monkey_self
+    else:
+        # Viewing somebody else's profile
+        monkey = Monkey.query.filter_by(id = monkey_id).first()
+
+    # The following lists keep logic out of the templates
+    # and keep the model simple at the same time:
+
+    mutual_friends = set(monkey.friends).intersection(monkey.friend_of)
+    other_friends = set(monkey.friends).difference(monkey.friend_of)
+    also_friend_of = set(monkey.friend_of).difference(monkey.friends)
+
+    return render_template('profile.html',
+                            monkey=monkey,
+                            monkey_self=monkey_self,
+                            mutual_friends=mutual_friends,
+                            other_friends=other_friends,
+                            also_friend_of=also_friend_of)
+
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
-    monkey = Monkey.query.filter_by(id = session['id']).first()
+    monkey_self = Monkey.query.filter_by(id = session['id']).first()
 
-    if request.form:
-        form = ProfileEditForm(request.form)
-    else:
-        form = ProfileEditForm(None, monkey)
+    # If no formdata is present in the request, the form is populated from the object:
+    form = ProfileEditForm(request.form, monkey_self)
 
+    # Used in the template:
     edit_result = ''
     edit_result_class = ''
 
     if request.method == 'POST' and form.validate():
-        # Check for email uniqueness by querying for another monkey with the same address.
+        # Check for email uniqueness by querying for another monkey with the same address:
         if not Monkey.query.filter(and_(func.lower(Monkey.email) == func.lower(request.form['email']),
                                         Monkey.id != session['id'])).first():
             # If no duplicate found, populate the monkey object and commit:
-            form.populate_obj(monkey)
+            form.populate_obj(monkey_self)
 
             try:
-                if monkey.date_of_birth == '':
-                    monkey.date_of_birth = None
+                if monkey_self.date_of_birth == '':
+                    # Handling empty birth dates:
+                    monkey_self.date_of_birth = None
                 else:
-                    monkey.date_of_birth = parser.parse(request.form['date_of_birth']).date()
+                    # Handling different date formats:
+                    monkey_self.date_of_birth = parser.parse(request.form['date_of_birth']).date()
                 db.session.commit()
                 edit_result = 'Changed saved'
                 edit_result_class = 'message_ok'
@@ -113,7 +124,7 @@ def edit():
 
     return render_template('edit.html',
                            form=form,
-                           monkey=monkey,
+                           monkey_self=monkey_self,
                            edit_result=edit_result,
                            edit_result_class=edit_result_class)
 
