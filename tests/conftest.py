f67bbff665f61b9cdcb07313d831a38ef9a692eb
eugene-doe <dobryanskiy@mail.ru>
2014-12-31 03:46:07 +0200

    Tests, temporary configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 from MonkeyBook.models import db
 import pytest
 
-@pytest.yield_fixture(scope='session')
+@pytest.yield_fixture(scope='function')
 def session():
     """Begin a nested session to roll back the transaction once it is over."""
     db.session.begin_nested()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,21 @@
 from MonkeyBook import app
 from MonkeyBook.models import Monkey, db
+from MonkeyBook.forms import *
 from datetime import date
 import pytest, sqlalchemy_utils
+import MonkeyBook.dba as dba
 
+## This file requires database tables to be created, but does not require any data to be present.
+## The database should not contain monkeys with the following emails, because they are created
+## during the tests: test@email.com, billg@microsoft.com, rms@gnu.org
+
+## The tests cover all methods from the database access module.
+
+@pytest.mark.usefixtures('session')
 class TestClass:
 
-    @pytest.mark.usefixtures('session')
     def test_monkey_model(session):
         monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
-        
         db.session.add(monkey)
         db.session.commit()
 
@@ -18,3 +25,160 @@ class TestClass:
         assert type(monkey.password) == sqlalchemy_utils.types.password.Password
         assert monkey.email == 'test@email.com'
         assert monkey.age() == 0
+
+    def test_get_monkey_by_email(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+        
+        same_monkey = dba.get_monkey_by_email(monkey.email)
+
+        assert type(same_monkey) is Monkey
+        assert same_monkey.email == monkey.email
+
+    def test_get_monkey_by_id(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+        
+        same_monkey = dba.get_monkey_by_id(monkey.id)
+
+        assert type(same_monkey) is Monkey
+        assert same_monkey.id == monkey.id
+
+    def test_check_unique_email(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+
+        assert dba.check_unique_email(monkey.email) == False
+        assert dba.check_unique_email(monkey.email, monkey.id) == True
+
+    def test_register_monkey(session):
+        form = ProfileEditForm()
+        form['first_name'].data = 'Test'
+        form['last_name'].data = 'User'
+        form['email'].data = 'test@email.com'
+        form['date_of_birth'].data = ''
+        form['password'].data = '123'
+        form['confirm'].data = '123'
+
+        monkey = dba.register_monkey(form)
+
+        assert type(monkey) is Monkey
+        assert monkey.id > 0
+
+    def test_delete_monkey(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+
+        assert dba.delete_monkey(monkey) == True
+
+    def test_get_monkey_profile(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+
+        profile = dba.get_monkey_profile(monkey)
+
+        assert 'mutual_friends' in profile 
+        assert 'other_friends' in profile
+        assert 'also_friend_of' in profile
+
+    def test_edit_monkey_profile(session):
+        monkey = Monkey('Test', 'User', '123', 'test@email.com', str(date.today()))
+        db.session.add(monkey)
+        db.session.commit()
+
+        old_id = monkey.id
+
+        form = ProfileEditForm()
+        form['first_name'].data = 'Bill'
+        form['last_name'].data = 'Gates'
+        form['email'].data = 'billg@microsoft.com'
+        form['date_of_birth'].data = ''
+        form['password'].data = 'secret'
+        form['confirm'].data = 'secret'
+
+        assert dba.edit_monkey_profile(monkey, form) == True
+        assert monkey.id == old_id
+        assert monkey.first_name == 'Bill'
+        assert monkey.last_name == 'Gates'
+        assert type(monkey.password) == sqlalchemy_utils.types.password.Password
+        assert monkey.email == 'billg@microsoft.com'
+        assert monkey.age() == None
+
+        # Fails to rollback, cleanup:
+        db.session.delete(monkey)
+        db.session.commit()
+
+    def test_add_friend(session):
+        bill = Monkey('Bill', 'Gates', 'secret', 'billg@microsoft.com')
+        rich = Monkey('Richard', 'Stallman', 'freedom', 'rms@gnu.org')
+        db.session.add(bill, rich)
+        db.session.commit()
+
+        assert dba.add_friend(rich, bill) == True
+        assert bill in rich.friends
+        assert rich in bill.friend_of
+
+        # Fails to rollback, cleanup:
+        dba.delete_monkey(bill)
+        dba.delete_monkey(rich)
+
+    def test_remove_friend(session):
+        bill = Monkey('Bill', 'Gates', 'secret', 'billg@microsoft.com')
+        rich = Monkey('Richard', 'Stallman', 'freedom', 'rms@gnu.org')
+        rich.friends.append(bill)
+        db.session.add(bill, rich)
+        db.session.commit()
+
+        assert bill in rich.friends
+        assert rich in bill.friend_of
+        assert dba.remove_friend(rich, bill) == True
+        assert len(list(rich.friends)) == 0
+        assert len(list(bill.friend_of)) == 0
+
+        # Fails to rollback, cleanup:
+        dba.delete_monkey(bill)
+        dba.delete_monkey(rich)
+
+    def test_set_best_friend(session):
+        bill = Monkey('Bill', 'Gates', 'secret', 'billg@microsoft.com')
+        rich = Monkey('Richard', 'Stallman', 'freedom', 'rms@gnu.org')
+        rich.friends.append(bill)
+        db.session.add(bill, rich)
+        db.session.commit()
+
+        assert dba.set_best_friend(rich, bill) == True
+        assert rich.best_friend is bill
+        assert rich.best_friend_id == bill.id
+
+        # Fails to rollback, cleanup:
+        dba.delete_monkey(bill)
+        dba.delete_monkey(rich)
+
+    def test_clear_best_friend(session):
+        bill = Monkey('Bill', 'Gates', 'secret', 'billg@microsoft.com')
+        rich = Monkey('Richard', 'Stallman', 'freedom', 'rms@gnu.org')
+        rich.friends.append(bill)
+        rich.best_friend = bill
+        db.session.add(bill, rich)
+        db.session.commit()
+
+        assert dba.clear_best_friend(rich) == True
+        assert rich.best_friend == None
+        assert rich.best_friend_id == None
+
+        # Fails to rollback, cleanup:
+        dba.delete_monkey(bill)
+        dba.delete_monkey(rich)
+
+    def test_list_monkeys(session):
+        monkeys = dba.list_monkeys()
+
+        assert monkeys.has_prev is not None
+        assert monkeys.has_next is not None
+        assert monkeys.iter_pages() is not None
+        assert monkeys.page is not None
