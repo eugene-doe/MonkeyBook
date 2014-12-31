63ad18f47fabfac02c770dfd38072283727534eb
eugene-doe <dobryanskiy@mail.ru>
2014-12-31 07:08:14 +0200

    runserver.py modified for Heroku

--- runserver.py
+++ runserver.py
@@ -6,9 +6,11 @@ from os import environ
 from MonkeyBook import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    #HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = '0.0.0.0'
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        #PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, debug = True)
+    app.run(HOST, PORT) #, debug = True)
