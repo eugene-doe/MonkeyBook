5823666c2701b1bc9da0c42b124c97edb4224b9d
eugene-doe <dobryanskiy@mail.ru>
2014-12-31 06:24:17 +0200

    Initial Bootstrap setup

--- MonkeyBook/__init__.py
+++ MonkeyBook/__init__.py
@@ -3,9 +3,11 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from MonkeyBook.models import db
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/monkeydb'
 db.app = app
 db.init_app(app)
