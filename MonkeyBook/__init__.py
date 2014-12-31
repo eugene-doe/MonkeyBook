db981598c1bd9337ea450a91cef9ac5fa39e52a5
eugene-doe <dobryanskiy@mail.ru>
2014-12-31 06:34:25 +0200

    make it better

--- MonkeyBook/__init__.py
+++ MonkeyBook/__init__.py
@@ -8,7 +8,8 @@ from MonkeyBook.models import db
 
 app = Flask(__name__)
 Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/monkeydb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rcfkjeoyxrtceo:QBILLu1p7QLZEcKjqZu26w63hQ@ec2-107-20-166-127.compute-1.amazonaws.com:5432/dflcvf1qdmh8rs'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/monkeydb'
 db.app = app
 db.init_app(app)
 
